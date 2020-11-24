99f356ef56e339879ecff426197c773922b729f1
DNftl <david.neftel@edu.ece.fr>
2020-11-24 21:36:11 +0100

    d2

--- src/index.js
+++ src/index.js
@@ -8,20 +8,14 @@ import reportWebVitals from './reportWebVitals';
 import widgetMeteo from "./widgetMeteo";
 import locationMeteo from './locationMeteo.js';
 
-import widgetPays from './widgetPays.js';
-import infoPays from './infoPays.js';
+import widgetFlight from './widgetFlight.js';
+import infoFlight from './infoFlight.js';
 
 import widgetResultsMatch from './widgetResultsMatch.js';
 import infoMatch from './infoMatch.js';
 
-import widgetCourbesVentes from './widgetCourbesVentes.js';
-
-import Boutton from './App.js';
-
-
-
-
-
+import widgetGraphLine from './widgetGraphLine.js';
+import widgetGraphPie from './widgetGraphPie.js';
 
 
 
